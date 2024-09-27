8fa503059364a196fbe9590a2df82056d37b9ecd
shirish-vrit <shirish@vrittechnologies.com>
2024-09-27 18:03:25 +0545

    publishing prototype

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../lib/Button/Button";
+function App() {
+  return (
+    <div>
+      <Button>Test</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      react: "react",
+      "react-dom": "react-dom",
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      name: "vrit-design",
+      fileName: "vrit-design",
+      formats: ["es", "umd"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "jsx",
+        },
+      },
+    },
+  },
+});
