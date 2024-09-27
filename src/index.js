9146ccad670a20e9ad2fc0bd57e35a5c19ad81a3
shirish-vrit <shirish@vrittechnologies.com>
2024-09-27 13:05:04 +0545

    simple package check

---
+++ src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MyComponent = void 0;
+// src/index.ts
+var MyComponent_1 = require("./components/MyComponent");
+Object.defineProperty(exports, "MyComponent", { enumerable: true, get: function () { return __importDefault(MyComponent_1).default; } });
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// src/index.ts
+export { default as MyComponent } from "./components/MyComponent";
