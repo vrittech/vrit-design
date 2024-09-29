ee35b9a9326843757aa44101f5a737e191cd2185
shirish-vrit <shirish@vrittechnologies.com>
2024-09-29 17:15:08 +0545

    new working setup with tailwind config, vite and package.json

--- lib/Button/Button.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface ButtonProps {
-  children: React.ReactNode;
-}
-
-export function Button({ children }: ButtonProps) {
-  return <button>{children}</button>;
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import tsconfigPaths from "vite-tsconfig-paths";
+import dts from "vite-plugin-dts";
+import tailwindcss from "tailwindcss";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      react: "react",
-      "react-dom": "react-dom",
-    },
-  },
   build: {
     lib: {
-      entry: resolve(__dirname, "lib/main.ts"),
+      entry: resolve(__dirname, "./lib/index.ts"),
       name: "vrit-design",
-      fileName: "vrit-design",
-      formats: ["es", "umd"],
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom", "react/jsx-runtime"],
+      external: ["react", "react-dom", "tailwindcss"],
       output: {
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
           "react/jsx-runtime": "react/jsx-runtime",
+          tailwindcss: "tailwindcss",
         },
       },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
     },
   },
 });
