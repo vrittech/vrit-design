f2df5bbfa149d30887efc86ebe1d852f1307cfb8
shirish-vrit <shirish@vrittechnologies.com>
2024-09-29 15:56:39 +0545

    feat: vite config and relase file changed

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
-          "react/jsx-runtime": "jsx",
+          "react/jsx-runtime": "react/jsx-runtime",
         },
       },
     },
